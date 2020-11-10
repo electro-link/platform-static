c84238c4dcfe950dce03cd36b8ceff35519d9606
Kai Li Lim <kai@electro.aero>
2020-11-10 10:34:27 +0800

    Webpack

---
+++ src/components/AzureUpload3D70.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="upload-form">
+        <form @submit.prevent="handleSubmit">
+            <label>File location</label>
+            
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'upload-form'
+    }
+
+    handleSubmit() {
+        
+    }
+</script>
+
